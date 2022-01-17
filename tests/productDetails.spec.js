2dbb823dc3f86791ed0a075b756fc58452466da4
Maju Domenciano <juliadomenciano@hotmail.com>
2022-01-17 18:41:34 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,7 +28,13 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    expect(typeof productDetails).toBe('function')
+    expect(productDetails()).toEqual(expect.arrayContaining([]))
+    expect(productDetails().length).toEqual(2)
+    expect(typeof productDetails()[0]).toBe('object')
+    expect(productDetails('a','b')[0]).not.toEqual(productDetails('a','b')[1])
+    expect(productDetails('a','b')[0].details.productId).toBe('a123')
+
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
     // Teste se o retorno da função é um array.

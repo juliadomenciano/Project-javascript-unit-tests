4bc875c684bf1efacfbf146dbc47045e9e2fd744
Maju Domenciano <juliadomenciano@hotmail.com>
2022-09-04 14:58:03 -0300

    Modificacao no readme e refatoracao de alguns codigos

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -3,17 +3,6 @@
 
 const numbers = require('../src/numbers');
 
-/*
-  A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
-
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
-  Comportamento:
-    - numbers([2, 3, 4]); // Retorna: true
-    - numbers([2, 'errado', 5]); // Retorna: false
-
-*/
-
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
 
@@ -22,9 +11,5 @@ describe('2 - Implemente os casos de teste para a função `numbers`', () => {
     expect(numbers([1, 'a', 3])).toEqual(false);
     expect(numbers([' '])).toEqual(false);
 
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
   });
 });
